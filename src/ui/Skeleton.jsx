a1a5939c1e67533b6073fef3ce19065bc68b9f3d
Micco James T. Ang <miccojamesang9@gmail.com>
2025-09-25 23:59:45 -0700

    Create Skeleton.jsx

---
+++ src/ui/Skeleton.jsx
@@ -0,0 +1,10 @@
+export default function Skeleton() {
+  return (
+    <div className="card skeleton">
+      <div className="sk-img" />
+      <div className="sk-line w-80" />
+      <div className="sk-line w-60" />
+      <div className="sk-line w-40" />
+    </div>
+  );
+}
